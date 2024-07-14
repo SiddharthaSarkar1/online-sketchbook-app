28f4d71aa9fb2c0892ca0d9566c20ac77ba41832
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-07-14 23:13:49 +0530

    provided prod endpoint

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = process.env.NODE_ENV === "production" ? "https://sketchbook-server-sidd.onrender.com" : "http://localhost:8080";
 
 export const socket = io(URL);
